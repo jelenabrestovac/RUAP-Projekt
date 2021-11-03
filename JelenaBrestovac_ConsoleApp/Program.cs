79ba87a9b34963e50de12733b8edb93b8dba2c7b
unknown <nstipic@etfos.hr>
2021-11-03 18:09:50 +0100

    Console commited

---
+++ JelenaBrestovac_ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JelenaBrestovac_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Jelene Brestovac");
+            
+        }
+    }
+}
